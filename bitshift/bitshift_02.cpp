bc39831c753969051611f7d9657cf6ac480c9849
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-03-11 17:47:37 +0300

    Update bitshift_02.cpp

--- bitshift/bitshift_02.cpp
+++ bitshift/bitshift_02.cpp
@@ -5,10 +5,10 @@ int main()
 	int x = -1;
 	if ((x >> 1) == -1)	//C++17 Implementation defined. C++20 guaranteeed to be true
 	{
-		std::cout << "dogru\n"; 
+		std::cout << "true\n"; 
 	}
 	else
 	{
-		std::cout << "yanlis\n";
+		std::cout << "false\n";
 	}
 }
